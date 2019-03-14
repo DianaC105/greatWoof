26b8f1a3728807a887cdcb86594eeba8b824ed1f
Danielle Dees <d.danielle83@gmail.com>
2019-03-13 21:38:49 -0400

    cleaning up

--- parallax-template/js/login.js
+++ parallax-template/js/login.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
        function loginCust (userData) {
            $.post("/api/login", userData)
            .then(function () {
-               window.location.replace("/members");
+               window.location.replace("/requests");
                
            });
        };
